24f8c828f63901f7ee9064607ad84594ec0dae7e
margtr <margtr@gmail.com>
2020-08-07 10:11:44 +0800

    Initial Commit

---
+++ cat-switch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cat-switch
+//
+//  Created by Ting on 2020/8/7.
+//  Copyright © 2020 Ting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
